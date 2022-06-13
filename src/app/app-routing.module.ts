6b3875fb76b20c977849d5b9c9d04ec7179de80e
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-06-13 12:38:05 +0700

    feat: Assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,58 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './Components/about/about.component';
-import { HeaderComponent } from './Components/header/header.component';
 import { ProductsComponent } from './Components/products/products.component';
-import { ProductAddComponent } from './Components/product-add/product-add.component';
-
+import { ProductAddComponent } from './page/Admin/product-add/product-add.component';
+import { SigninComponent } from './Components/signin/signin.component';
+import { ListUserComponent } from './Components/list-user/list-user.component';
+import { WebsiteLayoutsComponent } from './page/clients/website-layouts/website-layouts.component';
+import { HomepageComponent } from './page/clients/homepage/homepage.component';
+import { BlogComponent } from './page/clients/blog/blog.component';
+import { WorkComponent } from './page/work/work.component';
+import { AdminLayoutComponent } from './page/Admin/admin-layout/admin-layout.component';
+import { DashboardComponent } from './page/Admin/dashboard/dashboard.component';
 const routes: Routes = [
-  {path: "", component: HeaderComponent},
-  {path:"products", component: ProductsComponent },
-  {path: "about", component: AboutComponent},
-  {path: "add", component: ProductAddComponent}
+  {path: "listProducts", component: ProductsComponent},
+  {path: "add", component: ProductAddComponent},
+  {
+    path: "",
+    component: WebsiteLayoutsComponent,
+    children: [
+      {
+        path: "", component: HomepageComponent
+      },
+      {
+        path: "blog", component: BlogComponent
+      },
+      {
+        path: "work", component: WorkComponent
+      }
+    ]
+  },
+  {
+    path: "admin",
+    component: DashboardComponent,
+    children: [
+      {
+        path: "add", component: ProductAddComponent
+      },
+      {
+        path: "", component: ProductsComponent 
+      }
+    ]
+  },
+  
+
+  // {path: '', component: WebsiteLayoutsComponent},{
+  //   path: 'home', children: [
+  //     {path:"products", component: ProductsComponent },
+  //     {path: "about", component: AboutComponent},
+  //     {path: "add", component: ProductAddComponent},
+  //     {path: "signin", component: SigninComponent},
+  //     {path: "listUser", component: ListUserComponent}
+  //   ]
+  // }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/header/header.component';
 import { ProductsComponent } from './Components/products/products.component';
 import { AboutComponent } from './Components/about/about.component';
 import { FormsModule } from '@angular/forms';
-import { ProductAddComponent } from './Components/product-add/product-add.component';
+import { ProductAddComponent } from './page/Admin/product-add/product-add.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { SigninComponent } from './Components/signin/signin.component';
-
-
+import { ListUserComponent } from './Components/list-user/list-user.component';
+import { WebsiteLayoutsComponent } from './page/clients/website-layouts/website-layouts.component';
+import { AdminLayoutComponent } from './page/Admin/admin-layout/admin-layout.component';
+import { HomepageComponent } from './page/clients/homepage/homepage.component';
+import { BlogComponent } from './page/clients/blog/blog.component';
+import { WorkComponent } from './page/work/work.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { DashboardComponent } from './page/Admin/dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ProductsComponent,
     AboutComponent,
     ProductAddComponent,
-    SigninComponent
+    SigninComponent,
+    ListUserComponent,
+    WebsiteLayoutsComponent,
+    AdminLayoutComponent,
+    HomepageComponent,
+    BlogComponent,
+    WorkComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
